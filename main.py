d6ff96fef10199daf486a735ee3eaedb72b776f1
shreyakhapekar24 <97623859+shreyakhapekar24@users.noreply.github.com>
2022-02-12 02:31:59 +0530

    Assignment No. 4

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import cv2
+import numpy as np 
 
-originalImage = cv2.imread('nature.jpg')
-grayImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
+#read image
+src = cv2.imread('butterfly.jpg', cv2.IMREAD_UNCHANGED)
+print(src.shape)
 
-(thresh, blackAndWhiteImage) = cv2.threshold(grayImage, 127, 255, cv2.THRESH_BINARY)
+# extract green channel
+blue_channel = src[:, :, 0]
+green_channel = src[:, :, 1]
+red_channel = src[:, :, 2]
 
-subtrctImage = grayImage - blackAndWhiteImage
 
-cv2.imshow('Black White Image', blackAndWhiteImage)
-cv2.imshow('Original Image', originalImage)
-cv2.imshow('Gray Image', grayImage)
-cv2.imshow('Subtracted Image', subtrctImage)
+# create empty image with same shape as that of src image
+blue_img = np.zeros(src.shape)
+green_img = np.zeros(src.shape)
+red_img = np.zeros(src.shape)
+
+# assign the green channel of src to empty image 
+green_img[:, :, 1] = green_channel
+blue_img[:, :, 0] = blue_channel
+red_img[:, :, 2] = red_channel
+
+# show image 
+cv2.imshow('green channel', green_img)
+cv2.imshow('blue channel', blue_img)
+cv2.imshow('red channel', red_img)
 
 cv2.waitKey(0)
-cv2.destroyAllWindows()
